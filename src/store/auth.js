d531d396e0d6d325f6268f49dc4961d84bdde917
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio.local>
2019-09-16 18:06:57 -0300

    adding auth actions in store

--- src/store/auth.js
+++ src/store/auth.js
@@ -7,19 +7,87 @@ axios.defaults.baseURL = "http://auth_api.test/api";
 
 export const auth = new Vuex.Store({
     state: {
+        token: localStorage.getItem("access_token") || null,
         foo: ""
     },
     mutations: {
         setFoo(state, payload) {
             state.foo = payload;
-        }
+        },
+        retrieveveToken(state, payload) {
+            state.token = payload;
+        },
+        destroyToken(state) {
+            state.token = null;
+        },
     },
     actions: {
         getFoo(context) {
             axios.get('/foo').then(response => {
                 context.commit("setFoo", response.data);
             }).catch(error => {});
-        }
+        },
+        retrieveToken(context, credentials) {
+            return new Promise((resolve, reject) => {
+                axios
+                    .post("/login", {
+                        username: credentials.username,
+                        password: credentials.password
+                    })
+                    .then(response => {
+                        const token = response.data.access_token;
+                        localStorage.setItem("access_token", token);
+                        context.commit("retrieveveToken", token);
+                        resolve(response);
+
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        },
+        register(context, data) {
+            return new Promise((resolve, reject) => {
+                axios
+                    .post("/register", {
+                        name: data.name,
+                        email: data.email,
+                        password: data.password
+                    })
+                    .then(response => {
+                        const token = response.data.access_token;
+                        localStorage.setItem("access_token", token);
+                        context.commit("retrieveveToken", token);
+                        resolve(response);
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        },
+        destroyToken(context) {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.token;
+            if (context.getters.loggedIn) {
+                return new Promise((resolve, reject) => {
+                    axios
+                        .post("/logout")
+                        .then(response => {
+                            localStorage.removeItem("access_token");
+                            context.commit("destroyToken");
+                            resolve(response);
+                        })
+                        .catch(error => {
+                            localStorage.removeItem("access_token");
+                            context.commit("destroyToken");
+                            reject(error);
+                        });
+                });
+            }
+        },
     },
-    getters: {}
+    getters: {
+        loggedIn(state) {
+            return state.token !== null;
+        }
+    }
 })
