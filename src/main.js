0ea278ecbfb045554a0f144cd886e18a4fe1f880
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio.local>
2019-09-16 18:06:16 -0300

    adding redirect and main layout

--- src/main.js
+++ src/main.js
@@ -1,12 +1,46 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+import Master from './components/layouts/Master.vue'
 import {
   auth
 } from './store/auth'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+Vue.config.productionTip = false;
+
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!auth.getters.loggedIn) {
+      next({
+        name: 'login',
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (auth.getters.loggedIn) {
+      next({
+        name: 'home',
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
 
 new Vue({
-  render: h => h(App),
-  store: auth
+  render: h => h(Master),
+  store: auth,
+  router: router,
 }).$mount('#app')
